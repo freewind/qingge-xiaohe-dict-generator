208da24e6cef1d49aa9f9a6f3457bcc64608e275
freewind <nowindlee@gmail.com>
2018-08-05 16:32:27 +0800

    done

---
+++ src/main/kotlin/example/xiaohe-reverse-table-generator.kt
@@ -0,0 +1,19 @@
+package example
+
+import github.freewind.xiaohe.chardecoder.XiaoHeCharDecoder
+import java.io.File
+import java.nio.charset.Charset
+
+private val qinggeReverseFile = File("data/generated/xiaohe_reverse_table.txt")
+
+fun main(args: Array<String>) {
+    val items = XiaoHeCharDecoder.charsAll.map {
+        ReverseTableItem(it.char, it.longestCode)
+    }
+    qinggeReverseFile.writeText(
+            items.joinToString("\n") { (char, code) -> "$char $code" },
+            Charset.forName("UTF-8")
+    )
+}
+
+data class ReverseTableItem(val char: Char, val code: String)
---
+++ src/main/kotlin/example/xiaohe-table-generator.kt
@@ -0,0 +1,32 @@
+package example
+
+import java.io.File
+import java.nio.charset.Charset
+
+data class QinggeItem(val code: String, val words: List<String>)
+
+private val luogeDictFile = File("data/luoge-main-dict.txt")
+private val qinggeTargetFile = File("data/generated/xiaohe_table.txt")
+
+fun main(args: Array<String>) {
+    val luogeItems = readWordCodeFromLuoge(luogeDictFile)
+    val qinggeItems = luogeItems.groupBy { it.code }.mapValues { items ->
+        items.value.sortedBy { it.index }
+    }.map { (code, words) ->
+        QinggeItem(code, words.map { it.word })
+    }
+    qinggeTargetFile.writeText(
+            qinggeItems.joinToString("\n") { "${it.code} ${it.words.joinToString(" ")}" },
+            Charset.forName("UTF-8")
+    )
+}
+
+private fun readWordCodeFromLuoge(file: File): List<LuogeItem> {
+    return file.readLines(Charset.forName("UTF-16")).filterNot { it.isEmpty() }
+            .map { line ->
+                val (word, code, index) = line.split("\t")
+                LuogeItem(word, code, index.toInt())
+            }
+}
+
+data class LuogeItem(val word: String, val code: String, val index: Int)
